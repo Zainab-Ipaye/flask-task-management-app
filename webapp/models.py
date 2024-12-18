a0cfaefbd69512cae5f6946c3a7def1497079da6
Zainab Ipaye <zipaye7@gmail.com>
2024-12-18 16:05:54 +0000

    Update Registration Route

--- webapp/models.py
+++ webapp/models.py
@@ -42,7 +42,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(10), nullable=False, default='user')  
     
     tasks = db.relationship('Task', foreign_keys='Task.created_by', backref='author', lazy=True)
--- webapp/routes.py
+++ webapp/routes.py
@@ -256,12 +256,14 @@ def list_tasks():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = generate_password_hash(form.password.data) #.decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
 
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created!', 'success')
+    else:
+        flash('Registration failed. Check your emails and/or password', 'danger')
         return redirect(url_for('main.login'))
     return render_template('register.html', form=form)
 
