7f2204d947ef43bbe46665fbdbacbff84dfc78e2
Zainab Ipaye <zipaye7@gmail.com>
2024-12-19 11:22:38 +0000

    Added validation so project end dat is after start date

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, IntegerField, SelectField, DateTimeField, DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, InputRequired, Length, Email, EqualTo, ValidationError
 from .models import User, Project #, Sprint
 from flask import current_app
 
@@ -16,21 +16,13 @@ class ProjectForm(FlaskForm):
     end_date = DateField('End Date', format='%Y-%m-%d', validators=[DataRequired("End Date is required")])
     submit = SubmitField('Create Project')
 
+    def validate_end_date(self, field):
+        """
+        Custom validator to ensure end_date is after start_date.
+        """
+        if self.start_date.data and field.data and field.data <= self.start_date.data:
+            raise ValidationError("End Date must be after the Start Date.")
 
-#class SprintForm(FlaskForm):
- #   name = StringField('Sprint Name', validators=[DataRequired()])
-  #  start_date = DateField('Start Date', format='%Y-%m-%d', validators=[DataRequired()])
-   # end_date = DateField('End Date', format='%Y-%m-%d', validators=[DataRequired()])
-
-    #project_id = SelectField('Project ID', coerce=coerce_to_int_or_none, validators=[DataRequired()])  
-    #submit = SubmitField('Create Sprint')
-
-    #def __init__(self, *args, **kwargs):
-      #  super().__init__(*args, **kwargs)
-      #  with current_app.app_context():
-        
-       #     self.project_id.choices =  [(None, 'Select Project')] + [
-       #         (project.id, project.name) for project in Project.query.all()]
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired("Username is required"), Length(min=5, max=100, message="Username must be at least 5 characters.")])
@@ -57,10 +49,15 @@ class TaskForm(FlaskForm):
     
     assigned_to = SelectField('Assigned To', coerce=coerce_to_int_or_none, validators=[DataRequired("Assigned To is required")])  
     project_id = SelectField('Project', coerce=coerce_to_int_or_none, validators=[DataRequired("Project is required")])
-    #task_sprint = SelectField('Sprint', choices=[], coerce=coerce_to_int_or_none)
 
     
-    hours_remaining = IntegerField('Hours Remaining', validators=[DataRequired("Hours Remaining is required")])
+    hours_remaining = IntegerField(
+        'Hours Remaining', 
+        validators=[
+            InputRequired("Hours Remaining is required")  # Replace DataRequired with InputRequired
+        ]
+    )
+
     submit = SubmitField('Create Task')
 
     def __init__(self, *args, **kwargs):
@@ -78,12 +75,4 @@ class TaskForm(FlaskForm):
             self.project_id.choices = [(None, 'Select Project')] + [
                 (project.id, project.name) for project in Project.query.all()
             ]
-
-    #def __init__(self, *args, **kwargs):
-     #   super().__init__(*args, **kwargs)
-      #  with current_app.app_context():
-
- #           self.task_sprint.choices = [(None, 'Select Sprint')] + [
-  #              (sprint.id, sprint.name) for sprint in Sprint.query.all()
-   #         ]
 
