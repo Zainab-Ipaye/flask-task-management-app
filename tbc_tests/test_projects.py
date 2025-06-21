80a2e131aa2a178eed440887d0fc64c5a737bef2
Zainab Ipaye <zipaye7@gmail.com>
2025-06-21 15:15:22 +0100

    Commit test separation

--- tbc_tests/test_projects.py
+++ tbc_tests/test_projects.py
@@ -21,7 +21,6 @@ class ProjectTests(unittest.TestCase):
         db.create_all()
         self.client = self.app.test_client()
 
-# Create test users
         self.user = self.create_user("user", "user@example.com", "UserPass123!", "user")
         self.admin = self.create_user(
             "admin", "admin@example.com", "AdminPass123!", "admin"
@@ -56,7 +55,6 @@ class ProjectTests(unittest.TestCase):
                 "start_date": "01-06-2024",
                 "end_date": "01-09-2024",
                 "status": "In Progress",
-#'submit': 'Create Project'
             },
             follow_redirects=True,
         )
@@ -96,7 +94,7 @@ class ProjectTests(unittest.TestCase):
             self.assertEqual(updated_project.name, "Updated Project")
 
     def test_delete_project(self):
-        self.login_as(self.admin)  
+        self.login_as(self.admin)
 
         with self.app_context:
             project = Project(
